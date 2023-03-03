e2c0692059ae35b06db31d07afc8085200a1ba3c
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-03-03 13:24:08 +0000

    create Movie section

---
+++ src/components/Home/Movies/index.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { useState } from 'react'
+import * as ToogleGroup from '@radix-ui/react-toggle-group'
+
+import { CardsCarousel } from '@/components/CardsCarousel'
+
+export interface MoviesProps {
+  popular?: {
+    id: number
+    original_title: string
+    release_date: string
+    backdrop_path: string
+    poster_path: string
+  }[]
+  topRated?: {
+    id: number
+    original_title: string
+    release_date: string
+    backdrop_path: string
+    poster_path: string
+  }[]
+  upcoming?: {
+    id: number
+    original_title: string
+    release_date: string
+    backdrop_path: string
+    poster_path: string
+  }[]
+  labels: string[]
+}
+
+export function MovieSection({
+  popular,
+  topRated,
+  upcoming,
+  labels,
+}: MoviesProps) {
+  const [label, setLabel] = useState(labels[0])
+
+  return (
+    <section className="flex flex-col gap-3 mb-6 last:mb-0">
+      <div className="flex items-center justify-between">
+        <span className="text-neutral-100 font-bold text-xl md:text-2xl">
+          Movies
+        </span>
+        <ToogleGroup.Root
+          className="flex items-center w-fit"
+          type="single"
+          defaultValue={label}
+          onValueChange={(value) => {
+            if (value) setLabel(value)
+          }}
+        >
+          {labels.map((label, index) => {
+            return (
+              <ToogleGroup.Item
+                className="bg-neutral-200 text-neutral-800 border border-neutral-800 first:rounded-tl first:rounded-bl last:rounded-br last:rounded-tr py-1 px-3 overflow-hidden radix-state-on:bg-emerald-500 radix-state-on:text-neutral-50"
+                key={index}
+                value={label}
+              >
+                {label}
+              </ToogleGroup.Item>
+            )
+          })}
+        </ToogleGroup.Root>
+      </div>
+      {label === 'popular' && <CardsCarousel movies={popular} />}
+      {label === 'top_rated' && <CardsCarousel movies={topRated} />}
+      {label === 'upcoming' && <CardsCarousel movies={upcoming} />}
+    </section>
+  )
+}
