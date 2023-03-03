7399aaf4d971449d3fcac3b6d0662db8ce331340
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-03-03 13:24:43 +0000

    Create footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import { BlogSVG, GithubSVG, LinkedInSVG } from '../icons'
+
+export function Footer() {
+  return (
+    <footer className="w-full h-16 bg-neutral-800 flex flex-col items-center justify-center">
+      <span className="text-neutral-400">Made by Tiago Araujo &copy; 2023</span>
+      <span className="flex items-center gap-2">
+        <Link target="_blank" href="https://github.com/TiagoAraujoDev">
+          <GithubSVG />
+        </Link>
+        <Link
+          target="_blank"
+          href="https://www.linkedin.com/in/tiago-muniz-de-araujo-2b5b8a89/"
+        >
+          <LinkedInSVG />
+        </Link>
+        <Link target="_blank" href="https://technewsblog.vercel.app/">
+          <BlogSVG />
+        </Link>
+      </span>
+    </footer>
+  )
+}
