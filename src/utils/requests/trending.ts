d10525670a0cdededc552d8641ad7794f38762ef
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-03-08 12:08:06 +0000

    Refactor type to variant

--- src/utils/requests/trending.ts
+++ src/utils/requests/trending.ts
@@ -5,12 +5,12 @@ import { api } from "@/lib/axios";
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
 export const getTrending = async (
-  type: string,
+  variant: string,
   period: string,
 ): Promise<AxiosResponse | undefined> => {
   try {
     const response = await api.get(
-      `trending/${type}/${period}?api_key=${apiKey}`,
+      `trending/${variant}/${period}?api_key=${apiKey}`,
     );
     return response;
   } catch (error: any) {
