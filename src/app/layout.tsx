ab999ea667596d167abac45ae284913cc3efd410
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-04-03 11:27:58 +0100

    Beggin to create authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Footer } from "@/components/Footer";
 import { Providers } from "@/Providers";
 
 import "./globals.css";
+// import { authToken } from "@/lib/axios/requests/authentication";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -16,7 +17,10 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-export default function RootLayout({ children }: LayoutProps) {
+export default async function RootLayout({ children }: LayoutProps) {
+  // const response = await authToken();
+  //
+  // console.log(response?.data);
   return (
     <html lang="en">
       <body className="text-white bg-neutral-900 scrollbar-thin scrollbar-thumb-emerald-500 scrollbar-track-neutral-300">
---
+++ src/lib/axios/requests/authentication.ts
@@ -0,0 +1,18 @@
+import { api } from "@/lib/axios/index";
+import { AxiosError, AxiosResponse } from "axios";
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+export const authToken = async (): Promise<AxiosResponse | undefined> => {
+  try {
+    const response = await api.get(
+      `authentication/token/new?api_key=${apiKey}`,
+    );
+
+    return response;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error);
+    }
+  }
+};
