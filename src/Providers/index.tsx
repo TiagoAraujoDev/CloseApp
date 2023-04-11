47533fb10ef7d84b26e37499f8a6e3610343134d
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-04-11 15:27:54 +0100

    Refactor: Remove unnecessary import

--- src/Providers/index.tsx
+++ src/Providers/index.tsx
@@ -2,7 +2,6 @@
 
 import { ReactNode } from 'react'
 import { QueryClientProvider } from 'react-query'
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { queryClient } from '@/lib/ReactQuery'
